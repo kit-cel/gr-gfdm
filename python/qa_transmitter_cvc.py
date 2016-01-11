5b55d7dce6977a3a1aee9b9062e0fdf0a6a4ee82
Andrej Rode <mail@andrejro.de>
2016-01-11 15:14:47 +0100

    [TASK] Add dynamic testing with python gfdm module

--- python/qa_transmitter_cvc.py
+++ python/qa_transmitter_cvc.py
@@ -21,47 +21,36 @@
 
 from gnuradio import gr, gr_unittest
 from gnuradio import blocks
-import gfdm_swig as gfdm
+import numpy as np
+import gfdm
+import gfdm_swig as gfdms
+
 
 class qa_transmitter_cvc (gr_unittest.TestCase):
 
     def setUp (self):
         self.tb = gr.top_block ()
 
-    def tearDown (self):
+    def tearDiown (self):
         self.tb = None
 
     def test_001_t (self):
         # set up fg
-        nsubcarrier = 4
-        ntimeslots = 4
+        nsubcarrier = 8
+        ntimeslots = 16
         filter_width = 2
         filter_alpha = 0.35
-        src_data = (1+1j,1-1j,
-                    -1-1j,-1+1j,
-                    1+1j,1-1j,
-                    1+1j,1+1j,
-                    -1-1j,1-1j,
-                    -1+1j,-1-1j,
-                    1+1j,1-1j,
-                    1+1j,1-1j)
-        expected_result = (-1.92239433e-01-0.06435655j,  -7.79145448e-02-1.42576238j,
-        -1.03862842e-01+0.06672548j,  -2.01631722e-01+0.47144911j,
-        -1.18147020e+00-0.59032j   ,   3.42442003e-01+0.06587994j,
-         4.74627623e-01+0.3040393j ,   7.49309212e-01+0.60528329j,
-        -7.18202886e-01-0.59032j   ,   6.36604711e-02+0.08897928j,
-        -8.45392403e-01-0.3040393j ,  -1.41490093e-01-0.15499258j,
-        -1.18147020e+00-0.06435655j,  -8.49657987e-04+0.28888835j,
-         4.74627623e-01-0.06672548j,  -7.88491259e-02+0.06027499j)
+        src_data = np.array([np.complex(np.random.choice([-1, 1]), np.random.choice([-1, 1])) for i in xrange(nsubcarrier*ntimeslots)])
+        expected_result = gfdm.modulation.gfdm_tx_fft2(src_data,'rrc',filter_alpha,ntimeslots,nsubcarrier,filter_width,1)
         src = blocks.vector_source_c(src_data)
-        tm = gfdm.transmitter_cvc(nsubcarrier,ntimeslots,filter_width,filter_alpha)
+        tm = gfdms.transmitter_cvc(nsubcarrier,ntimeslots,filter_width,filter_alpha)
         dst = blocks.vector_sink_c(vlen=nsubcarrier*ntimeslots)
         self.tb.connect(src,tm)
         self.tb.connect(tm,dst)
         self.tb.run ()
         # check data
         result_data = dst.data()
-        self.assertComplexTuplesAlmostEqual(expected_result, result_data, 6)
+        self.assertComplexTuplesAlmostEqual(expected_result, result_data, 2)
 
 
 if __name__ == '__main__':
