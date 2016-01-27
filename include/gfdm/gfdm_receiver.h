169cde308636ee8d62a74e8e065a11ca9cc0f51a
Andrej Rode <mail@andrejro.de>
2016-01-27 17:15:33 +0100

    gr-gfdm: gfdm-receiver: create kernel class gfdm-receiver and start filling header

--- include/gfdm/gfdm_receiver.h
+++ include/gfdm/gfdm_receiver.h
@@ -1,45 +1,61 @@
 /* -*- c++ -*- */
-/*  
+/*
  * Copyright 2016 Andrej Rode
- *  
+ *
  * This file is part of GNU Radio
- *  
+ *
  * GNU Radio is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
- *  
+ *
  * GNU Radio is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *  
+ *
  * You should have received a copy of the GNU General Public License
  * along with GNU Radio; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street,
  * Boston, MA 02110-1301, USA.
- */ 
+ */
 
-#ifndef INCLUDED_GFDM_UTILS_H
-#define INCLUDED_GFDM_UTILS_H
+#ifndef INCLUDED_GFDM_RECEIVER_H
+#define INCLUDED_GFDM_RECEIVER_H
 #endif
 
 #include <gfdm/api.h>
-#include <gnuradio/filter/firdes.h>
 #include <gnuradio/fft/fft.h>
+#include <gnuradio/gr_complex.h>
 
 namespace gr {
   namespace gfdm {
-
-      class GFDM_API rrc_filter_sparse {
-        private:
-          std::vector<gr_complex> d_filter_taps;
+    namespace kernel {
+      
+      class GFDM_API gfdm_receiver
+      {
+        protected:
+          int d_nsubcarrier;
+          int d_ntimeslots;
+          int d_filter_width;
+          int d_N;
+          int d_fft_len;
+          fft::fft_complex *d_in_fft;
+          gr_complex *d_in_fft_in;
+          gr_complex *d_in_fft_out;
+          fft::fft_complex *d_sc_ifft;
+          gr_complex *d_sc_ifft_in;
+          gr_complex *d_sc_ifft_out;
 
         public:
-          rrc_filter_sparse(int ntaps, double alpha, int filter_width, int nsubcarrier, int ntimeslots);
-          void get_taps(std::vector<gr_complex> &out);
-          ~rrc_filter_sparse();
-      };
+          gfdm_receiver();
+          ~gfdm_receiver();
+          
 
-  } /* namespace gfdm */
+
+      };
+    } /* namespace kernel */
+  } /* namespace filter */
 } /* namespace gr */
+
+
