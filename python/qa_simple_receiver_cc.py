receiver_kernel: fixed scaling error, simple_receiver: added receiver_sync_block, qa_simple_receiver: tests against pygfdm/gfdm_receiver, works now with "new" modulator

python/qa_simple_receiver_cc.py
@@ -22,6 +22,11 @@
 from gnuradio import gr, gr_unittest
 from gnuradio import blocks
 import gfdm_swig as gfdm
+from pygfdm.filters import get_frequency_domain_filter
+from pygfdm.gfdm_receiver import gfdm_demodulate_block
+from pygfdm.utils import get_random_qpsk
+import numpy as np
+import matplotlib.pyplot as plt
 
 class qa_simple_receiver_cc (gr_unittest.TestCase):
 
@@ -32,10 +37,22 @@ class qa_simple_receiver_cc (gr_unittest.TestCase):
         self.tb = None
 
     def test_001_t (self):
-        # set up fg
-        self.tb.run ()
-        # check data
+        alpha = .5
+        M = 8
+        K = 4
+        L = 2
+        taps = get_frequency_domain_filter('rrc', alpha, M, K, L)
+        data = get_random_qpsk(M * K)
+        src = blocks.vector_source_c(data)
+        mod = gfdm.simple_receiver_cc(M, K, L, taps)
+        dst = blocks.vector_sink_c()
 
+        self.tb.connect(src, mod, dst)
+        self.tb.run ()
+        res = np.array(dst.data())
+
+        ref = gfdm_demodulate_block(data, taps, K, M, L)
+        self.assertComplexTuplesAlmostEqual(ref, res, 5)
 
 if __name__ == '__main__':
     gr_unittest.run(qa_simple_receiver_cc, "qa_simple_receiver_cc.xml")